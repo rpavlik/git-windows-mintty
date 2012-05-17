ec4b9a8b985f8c1210e27d1b50c914a17243bb55
unknown <rpavlik@zapdos.vrac.iastate.edu>
2012-05-17 09:53:35 -0500

    Initial commit

---
+++ GitMintty.js
@@ -0,0 +1,29 @@
+
+var wshShell = new ActiveXObject("WScript.Shell");
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var dp0 = fso.GetParentFolderName(WScript.ScriptFullName);
+
+var quotePath = function(path) {
+	return "\"" + path + "\"";
+}
+
+var quotedAbsolutePath = function(relpath) {
+	return quotePath(fso.BuildPath(dp0, relpath));
+}
+
+var shellArgs = " --login -i";
+var minttyArgs = " --title \"Git Bash\"";
+
+if (WScript.Arguments.Length > 0) {
+	wshShell.CurrentDirectory = WScript.Arguments(0);
+} else {
+	wshShell.CurrentDirectory = wshShell.ExpandEnvironmentStrings("%USERPROFILE%");
+}
+
+var cmd = quotedAbsolutePath("bin\\mintty.exe") +
+	minttyArgs +
+	" --icon " + quotedAbsolutePath("etc\\git.ico") +
+	" --exec " + quotedAbsolutePath("bin\\sh.exe") +
+	shellArgs;
+
+wshShell.Run(cmd, 1, false);
